32709f1030e62c5ef01144c42117486e3a9036e6
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-04-18 14:05:07 -0700

    My first commit

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 // Express
 const express = require('express');
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
 const routesAPI = require('./routes/routesAPI');
-const routesHTML = require('./routes/routesHTML');
+const routesHTML = require('./routes/routesHTML/index');
+
 app.use(express.static('public'));
-app.use('/', routesHTML);
 app.use('/api', routesAPI);
+app.use('/', routesHTML);
+
 
 //PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`App listening on: ${PORT}`));
