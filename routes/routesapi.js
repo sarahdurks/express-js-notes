8c2f8c6024c169a1fe85d3411fefc289891b405e
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-05-17 12:49:21 -0700

    update routes api  double check folder structure

--- routes/routesapi.js
+++ routes/routesapi.js
@@ -1,30 +1,29 @@
-const router = require('express').Router();
-const uuid = require('uuid');
-const path = require('path');
-const fs = require ('fs');
+const router = require("express").Router();
+const uuid = require("uuid");
+const path = require("path");
+const fs = require("fs");
+let { notes } = require("../db/db.json");
+const { validateNoteType, addNewNote } = require("../library/notes");
 
-let {notes}= require('../db/db.json');
-const { validateNoteType, addNewNote } = require ('../library/notes');
-
-router.get('/notes', (req, res) => {
-   res.json(notes);
+router.get("/notes", (req, res) => {
+  res.json(notes);
 });
 
-router.post('/notes', (req, res) => {
-    const newNote = {
-        id: uuid.v4(),
-        title: req.body.title,
-        text: req.body.text
-    }
-    if (!validateNoteType(newNote)) {
-        return res.status(400).send('Add text, title, and body content to your note!')
-    }
-    else { addNewNote(newnote, notes);
-    
-    res.json(notes);
-    }
- });
- 
- module.exports = router;
+router.post("/notes", (req, res) => {
+  const newNote = {
+    id: uuid.v4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  if (!validateNoteType(newNote)) {
+    return res
+      .status(400)
+      .send("Add text, title, and body content to your note!");
+  } else {
+    addNewNote(newnote, notes);
 
- 
+    res.json(notes);
+  }
+});
+// come back to add delete when working
+module.exports = router;
