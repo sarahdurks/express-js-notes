b6cba0c0f7e2c9645edb7744a22ea3cba85185df
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-05-26 15:18:37 -0700

    new examples to placehold, cleanup spacing on icons test

--- routes/routesapi.js
+++ routes/routesapi.js
@@ -1,48 +1,43 @@
 const router = require("express").Router();
-let { notes } = require("../db/db.json");
+let {
+  notes
+} = require("../db/db.json");
 const uuid = require("uuid");
 const path = require("path");
 const fs = require("fs");
-const { validateNoteType, addNewNote } = require("../library/notes");
-
-router.get('/notes', (req, res) => {
+const {
+  validateNoteType,
+  addNewNote
+} = require("../library/notes");
+router.get("/notes", (req, res) => {
   console.log(notes);
   res.json(notes);
 });
-// add new note with uuid
-router.post('/notes', (req, res) => {
+// add new note with uuid - makes the note have a unique id to
+router.post("/notes", (req, res) => {
   const newNote = {
     id: uuid.v4(),
     title: req.body.title,
     text: req.body.text
   }
-  if (!validateNoteType(newNote)) {
-    return res
-      .status(400)
-      .send("Add text, title, and body content to create your note!");
+  if(!validateNoteType(newNote)) {
+    return res.status(400).send("Add text, title, and body content to create your note!");
   } else {
     addNewNote(newNote, notes);
     res.json(notes);
   }
 });
 // delete note by ID
-router.delete('/notes/:id', (req, res) => {
-    const exists = notes.some(notes => notes.id === req.params.id);
-    if (exists) {
-        notes = notes.filter(note => note.id !== req.params.id);
-        fs.writeFileSync(
-                path.join(__dirname, '../db/db.json'),
-                JSON.stringify({ notes }, null, 2)
-
-              );
-
-        res.json(notes);
-    } else {
-        res.status(400).send("No matching note found, sorry!")
-
-    }
-
-
-
+router.delete("/notes/:id", (req, res) => {
+  const exists = notes.some(notes => notes.id === req.params.id);
+  if(exists) {
+    notes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify({
+      notes
+    }, null, 2));
+    res.json(notes);
+  } else {
+    res.status(400).send("No matching note found, sorry!")
+  }
 });
 module.exports = router;
--- routes/routeshtml.js
+++ routes/routeshtml.js
@@ -1,21 +1,16 @@
 // dependencies
-const path = require('path');
-const router = require('express').Router();
-
+const path = require("path");
+const router = require("express").Router();
 //home page
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/', 'index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/", "index.html"));
 });
 // notes page
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'notes.html'));
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "notes.html"));
 });
-
 // get all/any notes with wildcard
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
-  });
-  
-
-
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 module.exports = router;
